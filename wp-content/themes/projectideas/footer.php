7a21357ad0d28f045b4175f88f09cb977285e4ab
Gumbraise <vikiclashonyt@gmail.com>
2021-04-10 20:01:36 +0200

    Made header and footer

---
+++ wp-content/themes/projectideas/footer.php
@@ -0,0 +1,29 @@
+<footer class="page-footer dark">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <ul>
+                    <li><a href="find.html">Toutes les idées</a></li>
+                    <li><a href="find.html">Idées du jour</a></li>
+                    <li><a href="#">Recherche avancée</a></li>
+                    <li><a href="#">Recherche par utilisateur</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-3">
+                <ul>
+                    <li><a href="page.html">Vie privée</a></li>
+                    <li><a href="page.html">CGU</a></li>
+                    <li><a href="contactez-nous.html">Signaler un bug</a></li>
+                    <li><a href="contactez-nous.html">Contactez-nous</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <p>© 2021 ProjectIdeas</p>
+    </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+
+</html>
---
+++ wp-content/themes/projectideas/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title><?php bloginfo('title') ?></title>
+    <?php wp_head(); ?>
+</head>
+
+<body>
+<nav class="navbar navbar-light navbar-expand-md navigation-clean-button">
+    <div class="container"><a href="index.html"><img src="assets/img/tech/ProjectIdeas1.svg" style="height: 40px;"/></a>
+        <button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span
+                    class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navcol-1">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item"></li>
+                <li class="nav-item"><a class="nav-link" href="find.html">Trouver une idée</a></li>
+                <li class="nav-item"><a class="nav-link" href="poster.html">Poster une idée</a></li>
+            </ul>
+            <span class="navbar-text actions"> <a class="login" href="index.html">Se déconnecter</a><a
+                        class="btn btn-light action-button" role="button" href="profil.html">Profil</a></span>
+        </div>
+    </div>
+</nav>
