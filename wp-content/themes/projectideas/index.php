c0c212fc06b198da9c8f7657a15e29e9100e9955
Gumbraise <vikiclashonyt@gmail.com>
2021-04-10 20:02:04 +0200

    Added index.php and style.css

--- wp-content/themes/projectideas/index.php
+++ wp-content/themes/projectideas/index.php
@@ -1,1 +1,25 @@
 <?php
+get_header();
+?>
+<main class="page landing-page">
+	<section class="clean-block clean-hero" style="background-image: url(&quot;assets/img/tech/image4.jpg&quot;);color: #ffffff00;filter: blur(0px);">
+		<div class="text" style="filter: blur(0px);">
+			<h2 style="margin: 0;">ProjectIdeas</h2>
+			<p>Des idées pour vous les développeurs</p>
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						<form action="find.html">
+							<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Mod Spigot...">
+						</form>
+
+					</div>
+				</div>
+			</div>
+
+
+		</div>
+	</section>
+</main>
+<?php
+get_footer();
