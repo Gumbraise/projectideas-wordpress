68f6c8325c0b9b2776cd80667b70ae0c978f6718
Gumbraise <vikiclashonyt@gmail.com>
2021-04-10 20:28:21 +0200

    Improved homepage

--- wp-content/themes/projectideas/header.php
+++ wp-content/themes/projectideas/header.php
@@ -4,15 +4,20 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title><?php bloginfo('title') ?></title>
-    <?php wp_head(); ?>
+    <title><?php bloginfo( 'title' ) ?></title>
+	<?php wp_head(); ?>
 </head>
 
 <body>
 <nav class="navbar navbar-light navbar-expand-md navigation-clean-button">
-    <div class="container"><a href="index.html"><img src="assets/img/tech/ProjectIdeas1.svg" style="height: 40px;"/></a>
-        <button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span
-                    class="navbar-toggler-icon"></span></button>
+    <div class="container">
+        <a href="index.html">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/tech/ProjectIdeas1.svg" style="height: 40px;"/>
+        </a>
+        <button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="navbar-toggler-icon"></span>
+        </button>
         <div class="collapse navbar-collapse" id="navcol-1">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item"></li>
--- wp-content/themes/projectideas/index.php
+++ wp-content/themes/projectideas/index.php
@@ -1,25 +1,30 @@
 <?php
 get_header();
 ?>
-<main class="page landing-page">
-	<section class="clean-block clean-hero" style="background-image: url(&quot;assets/img/tech/image4.jpg&quot;);color: #ffffff00;filter: blur(0px);">
-		<div class="text" style="filter: blur(0px);">
-			<h2 style="margin: 0;">ProjectIdeas</h2>
-			<p>Des idées pour vous les développeurs</p>
-			<div class="container">
-				<div class="row">
-					<div class="col-12">
-						<form action="find.html">
-							<input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Mod Spigot...">
-						</form>
+    <main class="page landing-page">
+        <section class="section main-banner" id="top" data-section="section1">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/tech/image4.jpg" alt="" id="bg-video"
+                 style="width: 100vh; height: 100vh;">
 
-					</div>
-				</div>
-			</div>
-
-
-		</div>
-	</section>
-</main>
+            <div class="video-overlay header-text">
+                <div class="caption">
+                    <div class="text" style="filter: blur(unset);">
+                        <h2 style="margin: 0;">ProjectIdeas</h2>
+                        <p>Des idées pour vous les développeurs</p>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-12">
+                                    <form action="find.html">
+                                        <input type="text" class="form-control" id="exampleFormControlInput1"
+                                               placeholder="Mod Spigot...">
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 <?php
 get_footer();
