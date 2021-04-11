9f945bae371d45bc54d089834a38928703b1a7be
Gumbraise <vikiclashonyt@gmail.com>
2021-04-11 22:38:29 +0200

    Improved homepage style

--- wp-content/themes/projectideas/index.php
+++ wp-content/themes/projectideas/index.php
@@ -2,24 +2,27 @@
 get_header();
 ?>
     <main class="page landing-page">
-        <section class="section main-banner" id="top" data-section="section1">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/tech/image4.jpg" alt="" id="bg-video"
-                 style="width: 100vh; height: 100vh;">
-
-            <div class="video-overlay header-text">
-                <div class="caption">
-                    <div class="text" style="filter: blur(unset);">
-                        <h2 style="margin: 0;">ProjectIdeas</h2>
-                        <p>Des idées pour vous les développeurs</p>
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-12">
-                                    <form action="find.html" class="homepage">
-                                        <input type="text" class="form-control"
-                                               placeholder="Mod Spigot...">
-                                        <button class="button"><i class="fas fa-search"></i></button>
-                                    </form>
-
+        <section class="courses">
+            <div class="video-overlay"></div>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="section-heading">
+                            <div class="caption">
+                                <div class="text" style="filter: blur(unset);">
+                                    <h2 style="margin: 0;">ProjectIdeas</h2>
+                                    <p>Des idées pour vous les développeurs</p>
+                                    <div class="container">
+                                        <div class="row">
+                                            <div class="col-12">
+                                                <form action="find.html" class="homepage">
+                                                    <input type="text" class="form-control"
+                                                           placeholder="Mod Spigot...">
+                                                    <button class="button"><i class="fas fa-search"></i></button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -27,6 +30,47 @@ get_header();
                 </div>
             </div>
         </section>
+        <section>
+            <div class="container-fluid">
+                <div class="container cards-homepage">
+                    <div class="row">
+                        <div class="col-12 col-md-4 col-sm-6">
+                            <div class="card text-white bg-dark mb-3">
+                                <div class="card-header">Header</div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Dark card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make
+                                        up the bulk of the card's content.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 col-sm-6">
+                            <div class="card text-white bg-dark mb-3">
+                                <div class="card-header">Header</div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Dark card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make
+                                        up the bulk of the card's content.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 col-sm-6">
+                            <div class="card text-white bg-dark mb-3">
+                                <div class="card-header">Header</div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Dark card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make
+                                        up the bulk of the card's content.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
     </main>
 <?php
 get_footer();
