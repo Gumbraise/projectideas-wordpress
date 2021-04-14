515cb0f4e0aca9b6b9d4d47dc2369e668a2036bf
Gumbraise <vikiclashonyt@gmail.com>
2021-04-14 14:47:09 +0200

    Added linked card

--- wp-content/themes/projectideas/index.php
+++ wp-content/themes/projectideas/index.php
@@ -36,59 +36,72 @@ get_header();
                     <div class="owl-carousel">
 
                         <div class="card">
-                            <div class="card-header">Idée du jour</div>
-                            <div class="card-body">
-                                <h5 class="card-title">Mod Minecraft Spigot 1.8.9</h5>
-                                <p class="card-text">Bonjour, j'ai l'idée de créer un mod Minecraft pour Spigot
-                                    1.8.9 mais je n'en ai pas les compétances donc je viens ici pour donner l'idée
-                                    au personnes qualifiées pour...</p>
-                            </div>
-                            <div class="card-footer">
-                                <a href="#" style="float: right">Voir plus &raquo;</a>
-                            </div>
+                            <a href="#">
+                                <div class="card-header">Idée du jour</div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Mod Minecraft Spigot 1.8.9</h5>
+                                    <p class="card-text">Bonjour, j'ai l'idée de créer un mod Minecraft pour Spigot
+                                        1.8.9 mais je n'en ai pas les compétances donc je viens ici pour donner l'idée
+                                        au personnes qualifiées pour...</p>
+                                </div>
+                                <div class="card-footer">
+                                    <a href="#" style="float: right">Voir plus &raquo;</a>
+                                </div>
+                            </a>
                         </div>
+
+
                         <div class="card">
-                            <div class="card-header">Idée du mois</div>
-                            <div class="card-body">
-                                <h5 class="card-title">Mod Minecraft Spigot 1.8.9</h5>
-                                <p class="card-text">Bonjour, j'ai l'idée de créer un mod Minecraft pour Spigot
-                                    1.8.9 mais je n'en ai pas les compétances donc je viens ici pour donner l'idée
-                                    au personnes qualifiées pour...</p>
-                            </div>
-                            <div class="card-footer">
-                                <a href="#" style="float: right">Voir plus &raquo;</a>
-                            </div>
+                            <a href="#">
+                                <div class="card-header">Idée du mois</div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Mod Minecraft Spigot 1.8.9</h5>
+                                    <p class="card-text">Bonjour, j'ai l'idée de créer un mod Minecraft pour Spigot
+                                        1.8.9 mais je n'en ai pas les compétances donc je viens ici pour donner l'idée
+                                        au personnes qualifiées pour...</p>
+                                </div>
+                                <div class="card-footer">
+                                    <a href="#" style="float: right">Voir plus &raquo;</a>
+                                </div>
+                            </a>
                         </div>
+
+
                         <div class="card">
-                            <div class="card-header">Utilisateur de la semaine</div>
-                            <div class="card-body">
-                                <h5 class="card-title">Mod Minecraft Spigot 1.8.9</h5>
-                                <p class="card-text">Bonjour, j'ai l'idée de créer un mod Minecraft pour Spigot
-                                    1.8.9 mais je n'en ai pas les compétances donc je viens ici pour donner l'idée
-                                    au personnes qualifiées pour...</p>
-                            </div>
-                            <div class="card-footer">
-                                <a href="#" style="float: right">Voir son profil &raquo;</a>
-                            </div>
+                            <a href="#">
+                                <div class="card-header">Utilisateur de la semaine</div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Mod Minecraft Spigot 1.8.9</h5>
+                                    <p class="card-text">Bonjour, j'ai l'idée de créer un mod Minecraft pour Spigot
+                                        1.8.9 mais je n'en ai pas les compétances donc je viens ici pour donner l'idée
+                                        au personnes qualifiées pour...</p>
+                                </div>
+                                <div class="card-footer">
+                                    <a href="#" style="float: right">Voir son profil &raquo;</a>
+                                </div>
+                            </a>
                         </div>
+
                     </div>
                 </div>
             </div>
             <div class="container news">
                 <div class="row">
-                    <div class="card">
-                        <div class="card-header">Denières idées <sup>(actualisé toutes les 10 secondes)</sup>
+                    <a href="#">
+                        <div class="card">
+                            <div class="card-header">Denières idées <sup>(actualisé toutes les 10 secondes)</sup>
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Mod Minecraft Spigot 1.8.9</h5>
+                                <p class="card-text">Bonjour, j'ai l'idée de créer un mod Minecraft pour Spigot
+                                    1.8.9 mais je n'en ai pas les compétances donc je viens ici pour donner l'idée
+                                    au personnes qualifiées pour...</p>
+                            </div>
+                            <div class="card-footer">
+                                <a href="#" style="float: right">Voir plus &raquo;</a>
+                            </div>
                         </div>
-                        <div class="card-body">
-                            <h5 class="card-title">Mod Minecraft Spigot 1.8.9</h5>
-                            <p class="card-text">Bonjour, j'ai l'idée de créer un mod Minecraft pour Spigot
-                                1.8.9 mais je n'en ai pas les compétances donc je viens ici pour donner l'idée
-                                au personnes qualifiées pour...</p>
-                        </div>
-                        <div class="card-footer">
-                            <a href="#" style="float: right">Voir plus &raquo;</a>
-                        </div>
-                    </div>
+                    </a>
                 </div>
         </section>
 
