bc14398478b0f7182710ae2355ed73172893dae8
Gumbraise <vikiclashonyt@gmail.com>
2021-06-06 13:30:17 +0200

    ✨

---
+++ wp-content/themes/drg-theme/comments.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying comments
+ *
+ * This is the template that displays the area of the page that contains both the current comments
+ * and the comment form.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage DevsAreGenius
+ * @since DevsAreGenius 1.0
+ */
+
+/*
+ * If the current post is protected by a password and
+ * the visitor has not yet entered the password,
+ * return early without loading the comments.
+ */
+if ( post_password_required() ) {
+	return;
+}
+
+$comment_count = get_comments_number() + 1;
+?>
