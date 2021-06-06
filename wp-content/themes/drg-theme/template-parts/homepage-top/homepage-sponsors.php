9da9bfeed204a5d90c048993291d98fb90e021d3
Gumbraise <vikiclashonyt@gmail.com>
2021-06-06 16:55:22 +0200

    🐛

--- wp-content/themes/drg-theme/template-parts/homepage-top/homepage-sponsors.php
+++ wp-content/themes/drg-theme/template-parts/homepage-top/homepage-sponsors.php
@@ -22,32 +22,32 @@
 		<div class="lg:ml-20 grid grid-flow-col grid-rows-3 grid-cols-2">
 			<div class="h-48 flex items-center justify-center border-r border-b border-gray-500 border-opacity-25 border-solid">
 				<a href="https://www.instagram.com/kepi_working_legally/">
-					<img class="w-24" src="assets/img/sponsors/kepi_logo.svg" alt="Kepi logo">
+					<img class="w-24" src="<?php echo get_template_directory_uri() . '/assets/img/sponsors/kepi_logo.svg'; ?>" alt="Kepi Logotype">
 				</a>
 			</div>
 			<div class="h-48 flex items-center justify-center border-r border-b border-t border-gray-500 border-opacity-25 border-solid">
 				<a href="https://krbk.dev/">
-					<img class="w-24 rounded-xl" src="assets/img/sponsors/koro_logo.svg" alt="Koro logo">
+					<img class="w-24 rounded-xl" src="<?php echo get_template_directory_uri() . '/assets/img/sponsors/koro_logo.svg'; ?>" alt="Koro Mark">
 				</a>
 			</div>
 			<div class="h-48 flex items-center justify-center border-r border-t border-gray-500 border-opacity-25 border-solid">
 				<a href="https://dsc.bio/sparkot">
-					<img class="h-32" src="assets/img/sponsors/sparkot_logo.png" alt="Sparkot logo">
+					<img class="h-32" src="<?php echo get_template_directory_uri() . '/assets/img/sponsors/sparkot_logo.png'; ?>" alt="Sparkot Logotype">
 				</a>
 			</div>
 			<div class="h-48 flex items-center justify-center border-l border-b border-gray-500 border-opacity-25 border-solid">
 				<a href="https://palamazon.fr/">
-					<img class="w-36" src="assets/img/sponsors/palamazon_header.svg" alt="Palamazon logo">
+					<img class="w-36" src="<?php echo get_template_directory_uri() . '/assets/img/sponsors/palamazon_header.svg'; ?>" alt="Palamazon Logotype">
 				</a>
 			</div>
 			<div class="h-48 flex items-center justify-center border-l border-b border-t border-gray-500 border-opacity-25 border-solid">
 				<a href="https://kellis.fr/">
-					<img class="w-36" src="assets/img/sponsors/gumbraise_header.svg" alt="Gumbraise logo">
+					<img class="w-36" src="<?php echo get_template_directory_uri() . '/assets/img/sponsors/gumbraise_header.svg'; ?>" alt="Gumbraise Logotype">
 				</a>
 			</div>
 			<div class="h-48 flex items-center justify-center border-l border-t border-gray-500 border-opacity-25 border-solid">
 				<a href="https://quark-lang.dev/">
-					<img class="w-36" src="assets/img/sponsors/quark_header.svg" alt="Quark logo">
+					<img class="w-36" src="<?php echo get_template_directory_uri() . '/assets/img/sponsors/quark_header.svg'; ?>" alt="Quark Logotype">
 				</a>
 			</div>
 		</div>
