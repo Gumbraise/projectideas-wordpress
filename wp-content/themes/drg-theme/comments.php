7199a0ec71df343ebc4a87b3c684674c888bae4f
Gumbraise <vikiclashonyt@gmail.com>
2021-06-06 13:30:34 +0200

    ✨

--- wp-content/themes/drg-theme/comments.php
+++ wp-content/themes/drg-theme/comments.php
@@ -23,3 +23,23 @@ if ( post_password_required() ) {
 
 $comment_count = get_comments_number() + 1;
 ?>
+<div id="comments" class="container mx-auto px-4">
+    <div
+            class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg"
+    >
+        <div class="px-6 pb-10">
+            <div class=" mt-12">
+                <h4
+                        class="text-2xl font-semibold leading-normal mb-2 text-gray-800 mb-2"
+                >
+                    Comments
+                </h4>
+                <div class="mb-2 text-gray-700 mt-4">
+
+					<?php echo $comment_count;
+					if ( $comment_count <= 1 ) {
+						echo ' comment';
+					} else echo ' comments'
+					?>
+                </div>
+            </div>
