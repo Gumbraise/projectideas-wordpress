619a6ac5f0d738ad143e8ca4b3c7600d541a895a
Gumbraise <vikiclashonyt@gmail.com>
2021-06-06 23:14:43 +0200

    ✨

--- wp-content/themes/drg-theme/template-parts/homepage-contact.php
+++ wp-content/themes/drg-theme/template-parts/homepage-contact.php
@@ -36,78 +36,8 @@
 					class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300"
 				>
 					<div class="flex-auto p-5 lg:p-10">
-
-						<div class="relative w-full mb-3">
-							<label
-								class="block uppercase text-gray-700 text-xs font-bold mb-2"
-								for="full-name"
-							>Full Name</label
-							><input
-								id="full-name"
-								type="text"
-								class="border-0 px-3 py-3 placeholder-gray-400 text-gray-600 bg-white rounded text-sm shadow outline-none focus:outline-none focus:ring focus:ring-opacity-75 focus:ring-purple-900 w-full"
-								placeholder="Full Name"
-								style="transition: all 0.15s ease 0s;"
-							/>
-						</div>
-						<div class="relative w-full mb-3">
-							<label
-								class="block uppercase text-gray-700 text-xs font-bold mb-2"
-								for="email"
-							>Email</label
-							><input
-								id="email"
-								type="email"
-								class="border-0 px-3 py-3 placeholder-gray-400 text-gray-600 bg-white rounded text-sm shadow outline-none focus:outline-none focus:ring focus:ring-opacity-75 focus:ring-purple-900 w-full"
-								placeholder="Email"
-								style="transition: all 0.15s ease 0s;"
-							/>
-						</div>
-						<div class="relative w-full mb-3">
-							<label
-								class="block uppercase text-gray-700 text-xs font-bold mb-2"
-								for="email"
-							>Subject</label
-							>
-							<div class="relative flex flex-wrap items-stretch mb-3">
-								<select
-									id="contact-subject"
-									class="px-3 py-3 placeholder-purple-300 text-gray-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring focus:ring-opacity-75 focus:ring-purple-900 w-full"
-									style="transition: all 0.15s ease 0s;"
-									required
-								>
-									<option value="question" selected>Comment or question</option>
-									<option value="sponsor">I want to become a sponsor</option>
-									<option value="certified">I want my account to be certified</option>
-									<option value="feature">New feature</option>
-									<option value="report1">Report a problem</option>
-									<option value="report2">Report a problem / I got access trouble</option>
-									<option value="report3">Report a problem / The service is unavailable</option>
-									<option value="report4">Report a problem / An error occurs</option>
-									<option value="other">Other</option>
-								</select>
-							</div>
-						</div>
-						<div class="relative w-full mb-3">
-							<label
-								class="block uppercase text-gray-700 text-xs font-bold mb-2"
-								for="message"
-							>Message</label
-							><textarea
-								id="message"
-								rows="4"
-								cols="80"
-								class="border-0 px-3 py-3 placeholder-gray-400 text-gray-600 bg-white rounded text-sm shadow outline-none focus:outline-none focus:ring focus:ring-opacity-75 focus:ring-purple-900 w-full"
-								placeholder="Type a message..."
-								style="transition: all 0.15s ease 0s;"
-							></textarea>
-						</div>
-						<div class="text-center mt-6">
-							<button class="transition duration-200 bg-purple-900 text-gray-300 shadow hover:shadow-lg outline-none focus:outline-none uppercase hover:bg-purple-700 hover:text-white font-bold py-3 px-8 rounded inline-flex items-center">
-								Send Message
-							</button>
-						</div>
-					</div>
+						<?php echo do_shortcode( '[contact-form-7 id="16" title="Contact form 1"]' ); ?>
+                    </div>
 				</div>
 			</div>
 		</div>
