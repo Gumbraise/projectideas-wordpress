3fd38b2340eb86babd39cdaba979157157e5c5e3
Gumbraise <vikiclashonyt@gmail.com>
2021-06-08 14:48:58 +0200

    ✨

--- wp-content/themes/drg-theme/page-signin.php
+++ wp-content/themes/drg-theme/page-signin.php
@@ -7,16 +7,15 @@
  * @since 1.0.0
  */
 get_header();
-
 ?>
     <section class="relative w-full h-screen flex content-center items-center justify-center" style="min-height: 75vh;">
         <div
                 class="absolute top-0 w-full h-full bg-center bg-cover"
                 style="background-image: url(./assets/img/tech/image4.jpg);"
         >          <span
-                id="blackOverlay"
-                class="w-full h-full absolute opacity-50 bg-black"
-        ></span>
+                    id="blackOverlay"
+                    class="w-full h-full absolute opacity-50 bg-black"
+            ></span>
         </div>
         <div class="container mx-auto px-4 h-full">
             <div class="flex content-center items-center justify-center h-full">
@@ -32,22 +31,26 @@ get_header();
                             <hr class="mt-6 border-b-1 border-gray-400"/>
                         </div>
                         <div class="flex-auto px-4 lg:px-10 py-10 pt-0 py-12">
-                            <form>
+                            <form action="" method="post">
 
                                 <label
                                         class="block uppercase text-gray-700 text-xs font-bold mb-2"
-                                        for="grid-password"
-                                >Email</label
+                                        for="username-50"
+                                >Username or Email</label
                                 >
                                 <div class="relative flex w-full flex-wrap items-stretch w-full mb-3">
                                     <div class="z-10 h-full leading-snug font-normal absolute text-center text-purple-300 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3">
                                         <i class="fas fa-envelope text-purple-500"></i>
                                     </div>
                                     <input
-                                            type="email"
+                                            name="username-50"
+                                            id="username-50"
+                                            type="text"
                                             class="px-3 py-3 placeholder-purple-300 text-gray-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring focus:ring-opacity-75 focus:ring-purple-900 w-full pl-10"
-                                            placeholder="Email"
+                                            placeholder="Username or Email"
                                             style="transition: all 0.15s ease 0s;"
+                                            data-validate="unique_username_or_email"
+                                            data-key="username"
                                             required
                                     />
                                 </div>
@@ -62,29 +65,42 @@ get_header();
                                         <i class="fas fa-lock text-purple-500"></i>
                                     </div>
                                     <input
+                                            name="user_password-50"
+                                            id="user_password-50"
                                             type="password"
                                             class="px-3 py-3 placeholder-purple-300 text-gray-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring focus:ring-opacity-75 focus:ring-purple-900 w-full pl-10"
                                             placeholder="Password"
                                             style="transition: all 0.15s ease 0s;"
+                                            data-validate=""
+                                            data-key="user_password"
                                             required
                                     />
                                 </div>
                                 <div>
                                     <label class="inline-flex items-center cursor-pointer"
-                                    ><input
-                                            id="customCheckLogin"
-                                            type="checkbox"
-                                            class="form-checkbox border-0 rounded text-gray-800 ml-1 w-5 h-5"
-                                            style="transition: all 0.15s ease 0s;"
-                                    /><span class="ml-2 text-sm font-semibold text-gray-700"
-                                    >Remember me</span
-                                    ></label
+                                    >
+                                        <input
+                                                name="rememberme"
+                                                value="1"
+                                                id="customCheckLogin"
+                                                type="checkbox"
+                                                class="form-checkbox border-0 rounded text-gray-800 ml-1 w-5 h-5"
+                                                style="transition: all 0.15s ease 0s;"
+                                        /><span class="ml-2 text-sm font-semibold text-gray-700"
+                                        >Remember me</span
+                                        ></label
                                     >
                                 </div>
+                                <input type="hidden" name="form_id" id="form_id_50" value="50">
+                                <input type="hidden" name="um_request" id="um_request_50" class="input" value="" size="25" autocomplete="off">
+                                <input type="hidden" id="_wpnonce" name="_wpnonce" value="99d54a0aab">
+                                <input type="hidden" name="_wp_http_referer" value="/signin/">
                                 <div class="text-center mt-6">
-                                    <button class="w-full block text-center transition duration-200 bg-purple-900 text-gray-300 shadow hover:shadow-lg outline-none focus:outline-none uppercase hover:bg-purple-700 hover:text-white font-bold py-3 px-8 rounded items-center">
-                                        Sign in
-                                    </button>
+                                    <input
+                                            type="submit"
+                                            value="Sign in"
+                                            id="um-submit-btn"
+                                            class="w-full block text-center transition duration-200 bg-purple-900 text-gray-300 shadow hover:shadow-lg outline-none focus:outline-none uppercase hover:bg-purple-700 hover:text-white font-bold py-3 px-8 rounded items-center">
                                 </div>
                             </form>
                             <div class="flex flex-wrap mt-6">
