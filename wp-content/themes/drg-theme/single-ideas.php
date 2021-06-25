c4d2b0accf4bdd634620a51165d032f6d1a48f08
Gumbraise <vikiclashonyt@gmail.com>
2021-06-25 12:13:58 +0200

    Added scial media shre buttons

--- wp-content/themes/drg-theme/single-ideas.php
+++ wp-content/themes/drg-theme/single-ideas.php
@@ -78,6 +78,25 @@ gt_set_post_view();
                                 </p>
                             </div>
                         </div>
+                        <div class="text-center space-x-2">
+                            <a class="hover:text-white hover:bg-purple-600 transition duration-200 text-purple-600 shadow-lg border inline-block border-purple-600 font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none"
+                               target="_blank"
+                               href="https://www.facebook.com/dialog/share?app_id=2296148857110995&href=<?php echo home_url( $wp->request ); ?>"><i
+                                        class="pt-3 fax fa fa-facebook-f"
+                                        aria-hidden="true"></i></a>
+                            <a class="hover:text-white hover:bg-purple-600 transition duration-200 text-purple-600 shadow-lg border inline-block border-purple-600 font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none"
+                               target="_blank"
+                               href="https://www.linkedin.com/sharing/share-offsite/?url=<?php echo home_url( $wp->request ); ?>"><i
+                                        class="pt-3 fax fa fa-linkedin"
+                                        aria-hidden="true"
+                                ></i></a>
+                            <a class="hover:text-white hover:bg-purple-600 transition duration-200 text-purple-600 shadow-lg border inline-block border-purple-600 font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none"
+                               target="_blank"
+                               href="https://twitter.com/intent/tweet?url=<?php echo home_url( $wp->request ); ?>&text=<?php the_title(); ?>"><i
+                                        class="pt-3 fax fa fa-twitter"
+                                        aria-hidden="true"
+                                ></i></a>
+                        </div>
                     </div>
                 </div>
             </div>
--- wp-content/themes/drg-theme/single.php
+++ wp-content/themes/drg-theme/single.php
@@ -18,6 +18,26 @@ get_header();
 			</div>
 			<div class="p-5 m-4 bg-white relative shadow-sm rounded-3xl">
 				<?php the_content(); ?>
+
+                <div class="text-center space-x-2">
+                    <a class="hover:text-white hover:bg-purple-600 transition duration-200 text-purple-600 shadow-lg border inline-block border-purple-600 font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none"
+                       target="_blank"
+                       href="https://www.facebook.com/dialog/share?app_id=2296148857110995&href=<?php echo home_url( $wp->request ); ?>"><i
+                                class="pt-3 fax fa fa-facebook-f"
+                                aria-hidden="true"></i></a>
+                    <a class="hover:text-white hover:bg-purple-600 transition duration-200 text-purple-600 shadow-lg border inline-block border-purple-600 font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none"
+                       target="_blank"
+                       href="https://www.linkedin.com/sharing/share-offsite/?url=<?php echo home_url( $wp->request ); ?>"><i
+                                class="pt-3 fax fa fa-linkedin"
+                                aria-hidden="true"
+                        ></i></a>
+                    <a class="hover:text-white hover:bg-purple-600 transition duration-200 text-purple-600 shadow-lg border inline-block border-purple-600 font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none"
+                       target="_blank"
+                       href="https://twitter.com/intent/tweet?url=<?php echo home_url( $wp->request ); ?>&text=<?php the_title(); ?>"><i
+                                class="pt-3 fax fa fa-twitter"
+                                aria-hidden="true"
+                        ></i></a>
+                </div>
 			</div>
 		</div>
 	</section>
