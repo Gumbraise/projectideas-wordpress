49a2ba3f753d80905138e914e6db238a6835f976
Gumbraise <vikiclashonyt@gmail.com>
2021-06-25 22:51:20 +0200

    Removed code

--- wp-content/themes/drg-theme/page-users.php
+++ wp-content/themes/drg-theme/page-users.php
@@ -137,26 +137,8 @@ get_header();
 								echo $page;
 							}
 							echo '</nav>';
-
-						}
-						$from = ( $loop->query_vars['posts_per_page'] * $paged ) - ( $loop->query_vars['posts_per_page'] - 1 );
-						if ( ( $loop->query_vars['posts_per_page'] * $paged ) <= ( $loop->found_posts ) ) {
-							$to = ( $loop->query_vars['posts_per_page'] * $paged );
-						} else {
-							$to = $loop->found_posts;
 						}
 						?>
-                    </div>
-                    <div class="flex-1 flex items-center justify-center pt-8">
-                        <p class="text-sm text-gray-700">
-                            Showing
-                            <span class="font-medium"><?php echo $from; ?></span>
-                            to
-                            <span class="font-medium"><?php echo $to; ?></span>
-                            of
-                            <span class="font-medium"><?php echo count_users()['total_users']; ?></span>
-                            results
-                        </p>
                     </div>
                 </div>
             </div>
