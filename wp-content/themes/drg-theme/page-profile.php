367163d1148504e348c2c8db6f03ed910d5c2fe9
Gumbraise <vikiclashonyt@gmail.com>
2021-06-30 16:14:04 +0200

    added hsrae buttons on profile

--- wp-content/themes/drg-theme/page-profile.php
+++ wp-content/themes/drg-theme/page-profile.php
@@ -162,6 +162,26 @@ if ( $profile->ID == get_current_user_id() ) {
                                 </p>
                             </div>
                         </div>
+                        <div class="text-center space-x-2 mt-4">
+                            <a class="hover:text-white hover:bg-purple-600 transition duration-200 text-purple-600 shadow-lg border inline-block border-purple-600 font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none"
+                               target="_blank"
+                               href="https://www.facebook.com/dialog/share?app_id=2296148857110995&href=<?php echo home_url( $wp->request ); ?>"><i
+                                        class="pt-3 fax fa fa-facebook-f"
+                                        aria-hidden="true"></i></a>
+                            <a class="hover:text-white hover:bg-purple-600 transition duration-200 text-purple-600 shadow-lg border inline-block border-purple-600 font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none"
+                               target="_blank"
+                               href="https://www.linkedin.com/sharing/share-offsite/?url=<?php echo home_url( $wp->request ); ?>"><i
+                                        class="pt-3 fax fa fa-linkedin"
+                                        aria-hidden="true"
+                                ></i></a>
+                            <a class="hover:text-white hover:bg-purple-600 transition duration-200 text-purple-600 shadow-lg border inline-block border-purple-600 font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none"
+                               target="_blank"
+                               href="https://twitter.com/intent/tweet?url=<?php echo home_url( $wp->request ); ?>&text=<?php echo $profile->display_name; ?>"><i
+                                        class="pt-3 fax fa fa-twitter"
+                                        aria-hidden="true"
+                                ></i></a>
+                        </div>
+
                     </div>
                 </div>
             </div>
