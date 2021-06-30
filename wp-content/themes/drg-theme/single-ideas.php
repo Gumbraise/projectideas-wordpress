c0ce84fa9736c43c7585709d7e24deec6120456c
Gumbraise <vikiclashonyt@gmail.com>
2021-06-30 11:52:03 +0200

    protect ideas

--- wp-content/themes/drg-theme/single-ideas.php
+++ wp-content/themes/drg-theme/single-ideas.php
@@ -10,6 +10,9 @@ get_header();
 
 gt_set_post_view();
 
+global $post;
+$a_id = $post->post_author;
+
 ?>
     <section class="relative block" style="height: 500px;">
         <div
@@ -60,9 +63,7 @@ gt_set_post_view();
                         </h3>
                         <div class="mb-2 text-gray-700 mt-4">
                             <i class="fas fa-user mr-2 text-lg text-gray-500"></i
-                            ><a href="profil.html"><?php global $post;
-								$a_id = $post->post_author;
-
+                            ><a href="<?php echo home_url( 'profile/' . get_the_author_meta( 'user_nicename', $a_id )); ?>"><?php
 								the_author_meta( 'user_nicename', $a_id ); ?></a>
                         </div>
                         <div class="mb-2 text-gray-700">
@@ -72,9 +73,10 @@ gt_set_post_view();
                     </div>
                     <div class="mt-10 py-10 border-t border-gray-300 text-center">
                         <div class="flex flex-wrap justify-center">
-                            <div class="w-full lg:w-9/12 px-4">
-                                <p class="mb-4 text-lg leading-relaxed text-gray-800">
-									<?php echo htmlspecialchars( get_the_content() ); ?>
+                            <div class="w-full lg:w-9/12 px-4 relative">
+                                <div class="z-10 absolute w-full h-full block bg-gradient-to-b from-transparent via-white to-white"></div>
+                                <p class="mb-4 text-lg leading-relaxed text-gray-800 relative">
+									<?php if ( get_field( 'privacy' ) != 'private' ) { echo htmlspecialchars( get_the_content() ); } else echo htmlspecialchars( get_the_excerpt() ); ?>
                                 </p>
                             </div>
                         </div>
